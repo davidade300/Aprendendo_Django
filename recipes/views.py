5b13a351e57e853a84e3ffa92d70e694c8358a36
davidade300 <iaderinho@icloud.com>
2024-08-08 20:30:27 -0300

    templates para home e contato criados, fonte-awesome adicionado

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("HOME RECIPE")
+    # return HttpResponse("HOME RECIPE")
+    return render(request, "recipes/home.html", context={"name": "David"})
 
 
 def contato(request):
-    return HttpResponse("contato")
+    return render(request, "recipes/contato.html")
 
 
 def sobre(request):
